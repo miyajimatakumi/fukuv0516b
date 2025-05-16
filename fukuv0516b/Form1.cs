c85971ccc94b58a6e7a0e0c45893410ea8d33bd8
miyajimatakumi <m.miyajima.takumi@gmail.com>
2025-05-16 16:03:27 +0900

    v1

--- fukuv0516b/Form1.cs
+++ fukuv0516b/Form1.cs
@@ -6,5 +6,29 @@ namespace fukuv0516b
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left - 20;
+            label1.ForeColor = Color.Blue;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 20;
+            label1.ForeColor = Color.Blue;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 20;
+            label1.ForeColor = Color.Blue;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 20;
+            label1.ForeColor = Color.Blue;
+        }
     }
 }
